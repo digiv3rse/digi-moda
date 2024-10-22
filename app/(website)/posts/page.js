920ea871e96c15a3a56639ea8670faae37149ed5
DiGiV3rse <kimhabork@icloud.com>
2024-10-23 06:42:55 +0700

    Create page.js

---
+++ app/(website)/posts/page.js
@@ -0,0 +1,32 @@
+import { Suspense } from "react";
+import Container from "@/components/container";
+import Posts from "./posts";
+import Loading from "@/components/loading";
+
+export const dynamic = "force-dynamic";
+
+export const runtime = "edge";
+
+export default async function PostsPage({ searchParams }) {
+  return (
+    <>
+      <Container className="relative">
+        <h1 className="text-center text-3xl font-semibold tracking-tight dark:text-white lg:text-4xl lg:leading-snug">
+          Posts
+        </h1>
+        <div className="text-center">
+          <p className="mt-2 text-lg">
+            See all posts we have ever written.
+          </p>
+        </div>
+        <Suspense
+          key={searchParams.page || "5"}
+          fallback={<Loading />}>
+          <Posts searchParams={searchParams} />
+        </Suspense>
+      </Container>
+    </>
+  );
+}
+
+export const revalidate = 60;
