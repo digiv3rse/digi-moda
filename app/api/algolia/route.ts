fd5c371674336ec1146292f854b4154488154a97
digiv3rse <kimhabork@icloud.com>
2024-11-26 07:29:47 +0700

    update route.ts

--- app/api/algolia/route.ts
+++ app/api/algolia/route.ts
@@ -20,7 +20,7 @@ const sanity = createClient({
  *  This function receives webhook POSTs from Sanity and updates, creates or
  *  deletes records in the corresponding Algolia indices.
  */
-const handler = (req: VercelResponse, res: VercelResponse) => {
+export const handler = (req: VercelResponse, res: VercelResponse) => {
   // Tip: Add webhook secrets to verify that the request is coming from Sanity.
   // See more at: https://www.sanity.io/docs/webhooks#bfa1758643b3
   if (req.headers['content-type'] !== 'application/json') {
@@ -38,38 +38,19 @@ const handler = (req: VercelResponse, res: VercelResponse) => {
         index: algoliaIndex,
         projection: `{
           title,
-          "path": slug.current,
-          "body": pt::text(body)
-        }`,
-      },
-      // For the article document in this example we want to resolve a list of
-      // references to authors and get their names as an array. We can do this
-      // directly in the GROQ query in the custom projection.
-      article: {
-        index: algoliaIndex,
-        projection: `{
-          heading,
-          "body": pt::text(body),
-          "authorNames": authors[]->name
+          slug,
+          body[],
         }`,
       },
     },
 
     (document: SanityDocumentStub) => {
-      switch (document._type) {
-        case 'post':
+      if (document._type(post)) {
           return Object.assign({}, document, {
             custom: 'An additional custom field for posts, perhaps?',
           })
-        case 'article':
-          return {
-            title: document.heading,
-            body: document.body,
-            authorNames: document.authorNames,
-          }
-        default:
-          return document
-      }
+          return true
+        }
     },
     (document: SanityDocumentStub) => {
       if (document.hasOwnProperty('isHidden')) {
@@ -79,10 +60,6 @@ const handler = (req: VercelResponse, res: VercelResponse) => {
     }
   )
 
-  // Finally connect the Sanity webhook payload to Algolia indices via the
-  // configured serializers and optional visibility function. `webhookSync` will
-  // inspect the webhook payload, make queries back to Sanity with the `sanity`
-  // client and make sure the algolia indices are synced to match.
   return sanityAlgolia
     .webhookSync(sanity, req.body)
     .then(() => res.status(200).send('ok'))
@@ -93,5 +70,3 @@ const handler = (req: VercelResponse, res: VercelResponse) => {
       }
     })
 }
-
-export default handler;
