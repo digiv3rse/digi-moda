f446e52571a5e3c442e7959d5d87f74b02ff6264
digiv3rse <kimhabork@icloud.com>
2025-01-06 08:16:39 +0700

    update

--- app/(website)/gallery/page.js
+++ app/(website)/gallery/page.js
@@ -6,15 +6,15 @@ export default function GalleryPage() {
   return (
     <>
       <Container className="flex flex-col w-full mx-auto justify-center py-6 items-center md:py-10 md:mt-12 lg:mt-16 lg:py-14">
-        <div className="w-full sm:px-8 md:px-18 lg:px-24">
+       <div className="w-full h-full md:w-4/5">
           <Image
             src="/img/coming-soon-2.png"
             alt="Coming Soon"
-            className="object-cover w-full rounded-xl m-6 md:m-12 lg:m-16"
+            className="object-cover w-full rounded-xl"
           />
         </div>
        <Link href="/" className="flex items-center justify-center py-6 mx-auto text-center">
-          <button type="button" class="bg-[#5377b3] rounded-lg text-lg h-[45px] w-[90px] md:h-[60px] md:w-[135px] p-4 shadow-lg md:text-xl font-medium hover:bg-pink-600">Back Home</button>
+          <button type="button" class="bg-[#5377b3] rounded-lg text-lg p-4 shadow-lg md:text-xl font-medium hover:bg-pink-600">Back Home</button>
         </Link>
       </Container>
     </>
